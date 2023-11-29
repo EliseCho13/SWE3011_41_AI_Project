fc0f0c2d3cba3b80c465b1dab34c82507dfa9c81
EliseCho13 <dbwlspdlqj@naver.com>
2023-11-29 22:30:46 +0900

    fix: removed unnecessary comments

--- SWE3011_41_Task1.py
+++ SWE3011_41_Task1.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from datasets import load_dataset
 from sklearn.metrics import accuracy_score, classification_report
+
 # import sys
 # print("interpreter path is ", sys.executable) #python 설치 위치 확인
 # moduleNotFound에러 발생, pip install -U numpy scikit-learn scipy로 해결
@@ -22,14 +23,9 @@ def transform_data(X_train, X_test):
     - vectorizer: Fitted TfidfVectorizer object.
     """
     #########################################
-    # TF-IDF(Term Frequency-Inverse Document Frequency)는 단어의 빈도와 역 문서 빈도(문서의 빈도에 특정 식을 취함)를 사용하여 DTM 내의 각 단어들마다 중요한 정도를 가중치로 주는 방법
-    # TF와 IDF를 곱한 값.
     vectorizer = TfidfVectorizer()
-    # fit_transform()은 train dataset의 mean. variance를 학습시키는 데에 사용
     X_train_tfidf = vectorizer.fit_transform(X_train)
     # print("\nX_train_tfidf is \n", X_train_tfidf.toarray())
-    # test data까지 학습하면 성능에 영향을 주므로 구분해야 해서 fit_말고 그냥 transform 사용
-    # train data로부터 학습한 mean, variance를 test data에 적용하기 위해  transform사용
     X_test_tfidf = vectorizer.transform(X_test)
     #########################################
     return X_train_tfidf, X_test_tfidf, vectorizer
@@ -45,11 +41,8 @@ def logistic_regression(X_train_tfidf, y_train):
     - clf: Trained Logistic Regression model.
     """
     #########################################
-    clf = LogisticRegression(C=1, solver='lbfgs', penalty='l2',
-                             max_iter=1000)
+    clf = LogisticRegression(C=1, solver='lbfgs', penalty='l2', max_iter=1000)
     clf.fit(X_train_tfidf, y_train)
-    # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#sklearn.linear_model.LogisticRegression
-    # https://velog.io/@gayeon/%EB%8D%B0%EC%9D%B4%ED%84%B0-%EB%B6%84%EC%84%9D-%EC%B4%88%EB%B3%B4%EC%9E%90%EB%A5%BC-%EC%9C%84%ED%95%9C-Logistic-Regression-with-Scikit-Learn
     #########################################
     return clf
 
@@ -67,7 +60,6 @@ def random_forest(X_train_tfidf, y_train):
     clf = RandomForestClassifier(
         n_estimators=100, max_depth=None, min_samples_leaf=4, min_samples_split=2, n_jobs=-1)
     clf.fit(X_train_tfidf, y_train)
-    # https://zephyrus1111.tistory.com/249
     #########################################
     return clf
 
@@ -84,8 +76,6 @@ def naive_bayes_classifier(X_train_tfidf, y_train):
     #########################################
     clf = MultinomialNB(alpha=1.0)  # 0.1, 0.5, 1.0
     clf.fit(X_train_tfidf, y_train)
-    # https://todayisbetterthanyesterday.tistory.com/17
-    # https://todayisbetterthanyesterday.tistory.com/18
     #########################################
     return clf
 
@@ -103,7 +93,6 @@ def evaluate_model(clf, X_test_tfidf, y_test):
     #########################################
     y_pred = clf.predict(X_test_tfidf)
     accuracy = accuracy_score(y_test, y_pred)
-    # https://datainsider.tistory.com/53
     #########################################
     print(f"Accuracy: {accuracy:.2f}")
     print("Classification Report:")
